a1093b92f2d544d0646b6a80dd83d1b1d5356f25
pNuma <50904546+pNuma@users.noreply.github.com>
2025-11-12 01:51:37 +0900

    feat: create input field

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div id="question-display">
+      {{ question }}
+  </div>
+  <br>
+  
+  入力欄<br>
+  <input type="text" id="user-input">
 </template>
 
 <style scoped></style>
