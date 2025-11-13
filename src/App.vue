350ee90996c0e8c699848abde4de962abd64ae08
pNuma <50904546+pNuma@users.noreply.github.com>
2025-11-13 19:52:42 +0900

    feat: implement word list loading

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,33 @@
 <script setup lang="ts"></script>
 
+<script setup> 
+import { ref, onMounted } from 'vue';
+const question = ref('Now Loading...'); 
+let txt = "./wordlist.txt";
+let wordList = [];
+
+onMounted(() => {
+  fetch(txt)
+    .then(response => {
+      return response.text(); 
+    })
+    .then(data => {
+      wordList = data.split(/\n/);
+      question.value = wordList[0];
+    })
+    .catch(error => {
+      console.error('ファイルが読み込めません:', error);
+      question.value = 'お題が見つかりません';
+    });
+});
+</script>
+
 <template>
   <div id="question-display">
       {{ question }}
   </div>
   <br>
-  
+
   入力欄<br>
   <input type="text" id="user-input">
 </template>
