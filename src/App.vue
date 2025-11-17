453128e6b0898fbfdbc2c27437857c4c78b97afb
pNuma <50904546+pNuma@users.noreply.github.com>
2025-11-17 20:11:28 +0900

    feat: add romaji map and setup input handler

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,12 @@ const question = ref('Now Loading...');
 let txt = "./wordlist.txt";
 let wordList = [];
 
+const handleInput = (e) => {
+  const inputText = e.target.value; 
+  
+  console.log("今、入力された文字:", inputText);
+  }
+
 onMounted(() => {
   fetch(txt)
     .then(response => {
@@ -21,16 +27,40 @@ onMounted(() => {
       question.value = 'お題が見つかりません';
     });
 });
+
+
+// romajiMap を使って、カタカナ文字列をローマ字候補の配列に変換
+const convertRomaji = (word) => {
+  const result = [];
+  
+  for (let i = 0; i < word.length; i++) {
+    const char = word[i]; 
+    
+    const candidates = romajiMap[char];
+    
+    if (candidates) {
+      result.push(candidates);
+    } else {
+      result.push([char]);
+    }
+  }
+  
+  return result;
+};
+
 </script>
 
 <template>
   <div id="question-display">
-      {{ question }}
+    {{ question }} 
   </div>
+  
   <br>
-
+  
   入力欄<br>
-  <input type="text" id="user-input">
+  <input 
+    type="text" 
+    id="user-input"
+    @input="handleInput" 
+  >
 </template>
-
-<style scoped></style>
